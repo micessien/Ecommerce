4c1b9f3a96d7eaa8366ffcceffe7b34f693407e4
micessien <micaeldie@gmail.com>
2020-06-29 18:28:25 +0000

    Custom blade durectives to hide UI elements in Laravel by role

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,9 +33,11 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                        @hasrole('admin')
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('admin.users.index') }}">Manage Users</a>
                         </li>
+                        @endhasrole
                     </ul>
 
                     <!-- Right Side Of Navbar -->
