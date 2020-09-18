cf86eb575414351f8351ea1165a0d675f72d57c5
Micael Die <micaeldie@gmail.com>
2020-09-18 02:45:34 +0000

    Add verify to route and middleware to home route

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
 
 Route::get('/admin', function(){
     return 'you are admin';
