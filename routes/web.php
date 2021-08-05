1ad8133903a9169a144a5992f9f6cd66571231d9
micessien <micaeldie@gmail.com>
2021-08-05 01:38:23 +0000

    Cart Product Listing done

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,4 @@ Route::get('/', [ProductController::class, 'index']);
 Route::get('/detail/{detail}', [ProductController::class, 'detail']);
 Route::get('/search', [ProductController::class, 'search']);
 Route::post('/add_to_cart', [ProductController::class, 'addToCart']);
+Route::get('/cartlist', [ProductController::class, 'cartList']);
