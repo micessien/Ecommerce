524049a0501948aee7b8dd7377554a1050242420
micessien <micaeldie@gmail.com>
2021-08-05 02:08:57 +0000

    Order detail page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -61,4 +61,11 @@ class ProductController extends Controller
         Cart::destroy($id);
         return redirect('cartlist');
     }
+
+    function ordernow()
+    {
+        $userId = Session::get('user')['id'];
+        $total = DB::table('cart')->join('products', 'cart.product_id','=','products.id')->where('cart.user_id', $userId)->sum('products.price');
+        return view('ordernow', ['total'=>$total]);
+    }
 }
